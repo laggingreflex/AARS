07d3767b0b947cf936d75936029c144b9118a9b5
lg <laggingreflex@gmail.com>
2016-11-02 06:32:19 -0700

    karma

--- karma.conf.my.js
+++ karma.conf.my.js
@@ -1,25 +1,41 @@
 // Karma configuration
-// Generated on Wed Nov 02 2016 03:15:34 GMT-0700 (Pacific Daylight Time)
+// Generated on Wed Nov 02 2016 01:37:54 GMT-0700 (Pacific Daylight Time)
+
+const fs = require('fs');
 
 module.exports = function (config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
+    // basePath: '../..',
     basePath: '',
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
+    // frameworks: [ 'mocha' ],
+    // client: {
+    //   mocha: {
+    //     // change Karma's debug.html to the mocha web reporter
+    //     reporter: 'html'
+
+    //     // // require specific files after Mocha is initialized
+    //     // require: [require.resolve('bdd-lazy-var/bdd_lazy_var_global')],
+
+    //     // // custom ui, defined in required file above
+    //     // ui: 'bdd-lazy-var/global',
+    //   }
+    // },
+    frameworks: [ 'mocha' ],
 
     // list of files / patterns to load in the browser
     files: [
+      'src/**/tests/*.js',
       'src/**/*.test.js',
       'tests/**/*.js'
     ],
 
     // list of files to exclude
-    exclude: [
-    ],
+    exclude: [],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
@@ -28,10 +44,26 @@ module.exports = function (config) {
       'tests/**/*.js': [ 'babel' ]
     },
 
+    // babelPreprocessor: {
+    //   // options: JSON.parse(fs.readFileSync('.babelrc', 'utf8')),
+    //   options: {
+    //     presets: [
+    //       'es2015-node6/object-rest',
+    //       'stage-0',
+    //       'flow'
+    //     ],
+    //     'plugins': ['transform-es2015-modules-umd'],
+    //     env: {
+    //       test: {plugins: [ 'rewire' ]}
+    //     },
+    //     sourceMap: 'inline'
+    //   }
+    // },
+
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: [ 'progress' ],
 
     // web server port
     port: 9876,
@@ -41,6 +73,7 @@ module.exports = function (config) {
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    // logLevel: config.LOG_DEBUG,
     logLevel: config.LOG_INFO,
 
     // enable / disable watching file and executing tests whenever any file changes
@@ -48,7 +81,6 @@ module.exports = function (config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    // browsers: ['Chrome'],
     browsers: ['jsdom'],
 
     // Continuous Integration mode
